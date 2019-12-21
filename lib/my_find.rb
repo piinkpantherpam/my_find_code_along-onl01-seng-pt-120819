87a2e56183d8e78e1065f0d308d195565d92a71a
Pam Cairo <pcairo_90@outlook.com>
2019-12-21 05:10:36 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,7 +1,7 @@
 def my_find(collection)
-i = 0
+  i = 0
   while i < collection.length
-    return collection [i] if yield(collection[i]) return collection [1]
+    return collection [i] if yield(collection[i])
     i = i + 1
   end
 end
